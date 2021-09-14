4f4c321b12ed88b96255f88c0dd8faba1058c9b6
MedAliMarz <servfrdali@yahoo.fr>
2021-09-14 16:03:52 +0200

    added & configured eslint & prettier

--- App.js
+++ App.js
@@ -1,41 +1,45 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
     <>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Text>search</Text>
-      </View>
-      <View style={styles.list}>
-        <Text>list</Text>
-        
-      </View>
-    </SafeAreaView>
-    <ExpoStatusBar style='auto'/>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
-  search:{
+  search: {
     flexGrow: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  list:{
+  list: {
     flexGrow: 10,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-  }
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
